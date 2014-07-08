a9cd61be22d9eb27ed66876adf1dbab310b59f14
Prelang Builder <builder@prelang.com>
2014-07-08 00:05:45 +0000

    Defined validation for attribute 'presentation' on model 'Event'

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ActiveRecord::Base
   belongs_to :page_template
   has_and_belongs_to_many :speakers
   validates_formatting_of :video, using: :url
+  validates_formatting_of :presentation, using: :url
 end
